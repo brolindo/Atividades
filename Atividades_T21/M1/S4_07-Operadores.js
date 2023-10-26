01610e2323f71aa54015eb71e4413e6e42fc1143
Brolindo <brolinufmg@gmail.com>
2023-10-25 21:58:44 -0300

    Atividade Operadores JavaScript

---
+++ Atividades_T21/M1/S4_07-Operadores.js
@@ -0,0 +1,34 @@
+// Exercicio 01
+let num1 = 5;
+let num2 = 10;
+console.log(num1 + num2);
+
+// exercicio 02
+const pi = 3.14;
+let raio = 5;
+let perimetro = 2 * pi * raio;
+console.log(perimetro);
+
+//Exercício 04
+let nota1 = 7;
+let nota2 = 9;
+let media = (nota1 + nota2) / 2;
+console.log(media);
+
+//Exercicio 05
+let altura = 10;
+let largura = 10;
+let comprimento = 10;
+const area = largura * altura * comprimento;
+console.log(area);
+
+// exercicio 06
+let salarioBruto = 5000;
+let salarioLiquido = salarioBruto * 0.9;
+console.log(salarioLiquido);
+
+//Exercicio 07
+let horasTrabalhadas = 40;
+const valorHora = 50;
+salarioBruto = horasTrabalhadas * valorHora;
+console.log(salarioBruto);
